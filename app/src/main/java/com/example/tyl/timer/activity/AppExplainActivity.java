1bc2eea2df4316f7cb2504780f04a5a0c38b61b0
tianyl <962638847@qq.com>
2017-07-15 14:57:43 +0800

    补充说明

--- app/src/main/java/com/example/tyl/timer/activity/AppExplainActivity.java
+++ app/src/main/java/com/example/tyl/timer/activity/AppExplainActivity.java
@@ -21,7 +21,7 @@ public class AppExplainActivity extends AppCompatActivity {
         mTextTips = (TextView) findViewById(R.id.app_tips);
         mToolbar = (Toolbar) findViewById(R.id.explan_toolbar);
         mToolbar.setTitle("使用说明:");
-        mTextView.setText("时间计划器是一个可以制定计划并统计完成情况的应用,利用它你能看到自己的规划多少得到了实施.希望能督促它你尽可能达成自己的规划,享受自律带来的" +
+        mTextView.setText("事务规划器是一个可以制定计划并统计完成情况的应用,利用它你能看到自己的规划多少得到了实施.希望能督促它你尽可能达成自己的规划,享受自律带来的" +
                 "自信.");
         mTextTips.setText("注意: 本应用需要开启通知栏显示才能使用,请打开手机 设置-通知和状态栏 开启本应用的通知许可;因为各手机厂商的不同,下面图标的最后三个通知图标" +
                 "可能无法正常显示(比如小米通知栏可能只显示应用的图标)，功能正常使用。");
